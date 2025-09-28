d2b1bc14166f76447025a0256e8c219a1f1ecbfa
Mariem2Mostafa <mariem.19.1.2002@gmail.com>
2025-09-28 22:50:38 +0300

    update viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
-    tailwindcss(),
-  ],
+    tailwindcss(),],
+  base: process.env.VITE_API_BASE_URL
 })
